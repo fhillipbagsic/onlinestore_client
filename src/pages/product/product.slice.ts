2f3ca7749195832f2c7ef398a2b28ffb314091ed
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-10-24 18:01:39 +0800

    Remove unused imports

--- src/pages/product/product.slice.ts
+++ src/pages/product/product.slice.ts
@@ -1,47 +1,45 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios'
-import { ENDPOINT } from '../../utils/endpoint'
-import { DefaultState } from '../../utils/interfaces/defaultstate.interface'
-import ProductInterface from '../../utils/interfaces/product.interface'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { ENDPOINT } from "../../utils/endpoint";
+import { DefaultState } from "../../utils/interfaces/defaultstate.interface";
+import ProductInterface from "../../utils/interfaces/product.interface";
 
 interface ProductState extends DefaultState {
-    product: ProductInterface | {}
+  product: ProductInterface | {};
 }
 
 const initialState: ProductState = {
-    state: 'idle',
-    error: '',
-    product: {},
-}
+  state: "idle",
+  error: "",
+  product: {},
+};
 
 const productSlice = createSlice({
-    name: 'product',
-    initialState: initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(getProductById.pending, (state, action) => {
-            state.state = 'fetching'
-        })
-        builder.addCase(getProductById.fulfilled, (state, action) => {
-            state.product = action.payload
-            state.state = 'idle'
-        })
-        builder.addCase(getProductById.rejected, (state, action) => {
-            state.error = 'Something went wrong.'
-            state.state = 'error'
-        })
-    },
-})
-
-export const {} = productSlice.actions
+  name: "product",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getProductById.pending, (state, action) => {
+      state.state = "fetching";
+    });
+    builder.addCase(getProductById.fulfilled, (state, action) => {
+      state.product = action.payload;
+      state.state = "idle";
+    });
+    builder.addCase(getProductById.rejected, (state, action) => {
+      state.error = "Something went wrong.";
+      state.state = "error";
+    });
+  },
+});
 
 export const getProductById = createAsyncThunk(
-    'product/getProductById',
-    async (id: number) => {
-        const response = await axios.get(`${ENDPOINT}/products/${id}`)
+  "product/getProductById",
+  async (id: number) => {
+    const response = await axios.get(`${ENDPOINT}/products/${id}`);
 
-        return response.data
-    }
-)
+    return response.data;
+  }
+);
 
-export default productSlice.reducer
+export default productSlice.reducer;
